21d1f6f062b427ea8c449f242ade31901fa551b2
lixianmin01 <lixianmin01@xmli.local>
2022-01-30 15:42:54 +0800

    加入Rate1.vue；修改.gitignore

--- src/App.vue
+++ src/App.vue
@@ -2,11 +2,17 @@
 // This starter template is using Vue 3 <script setup> SFCs
 // Check out https://v3.vuejs.org/api/sfc-script-setup.html#sfc-script-setup
 import HelloWorld from './components/HelloWorld.vue'
+
+import Rate from './components/Rate1.vue'
+import {ref} from "vue";
+let score = ref(4)
+
 </script>
 
 <template>
   <img alt="Vue logo" src="./assets/logo.png" />
   <HelloWorld msg="Hello Vue 3 + Vite" />
+  <Rate :value="score"></Rate>
 </template>
 
 <style>
